6160f3d6067b627e6aa72e6ce57f76b4af2acfd5
Aniruth-Dev2006 <aniruthrajagopal2006@gmail.com>
2026-06-06 14:22:32 +0530

    Fixed tech stack visibility by base64-encoding SVGs to bypass GitHub sanitizer

--- generate_carousel.py
+++ generate_carousel.py
@@ -1,31 +1,45 @@
 import urllib.request
+import base64
 
-url = "https://skillicons.dev/icons?i=py,java,js,c,cpp,mysql,mongodb,tensorflow,sklearn,react,nextjs,nodejs,fastapi,docker,aws,html,css,tailwind,linux"
-req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-response = urllib.request.urlopen(req).read().decode('utf-8')
+icons = [
+    "py", "java", "js", "c", "cpp", "mysql", "mongodb", "tensorflow",
+    "sklearn", "react", "nextjs", "nodejs", "fastapi", "docker", "aws",
+    "html", "css", "tailwind", "linux"
+]
 
-# Extract inner SVG content
-inner_svg = response[response.find('<svg'):]
-inner_content = inner_svg[inner_svg.find('>')+1 : inner_svg.rfind('</svg>')]
-
-# Fix IDs in the duplicated content so they don't conflict (skillicons uses things like id="react-dark")
-dup_content = inner_content.replace('id="', 'id="dup_').replace('url(#', 'url(#dup_')
-
-svg_content = f"""<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 800 80" width="800" height="80">
+svg_content = """<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 800 80" width="800" height="80">
   <rect width="100%" height="100%" fill="#0d1117" rx="10" />
   <g>
     <animateTransform attributeName="transform" type="translate" from="0 0" to="-1216 0" dur="20s" repeatCount="indefinite"/>
-    <svg x="0" y="16" width="1216" height="48">
-      {inner_content}
-    </svg>
-    <svg x="1216" y="16" width="1216" height="48">
-      {dup_content}
-    </svg>
-  </g>
-</svg>
 """
+
+x_offset = 0
+b64_cache = []
+
+# Fetch and base64 encode each icon
+for icon in icons:
+    url = f"https://skillicons.dev/icons?i={icon}"
+    req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    response = urllib.request.urlopen(req).read()
+    
+    # Base64 encode the SVG response
+    b64_data = base64.b64encode(response).decode('utf-8')
+    b64_cache.append(b64_data)
+    
+    # Add to SVG content
+    svg_content += f'    <image x="{x_offset}" y="16" width="48" height="48" href="data:image/svg+xml;base64,{b64_data}" />\n'
+    x_offset += 64
+
+# Duplicate for infinite scroll
+x_offset = 1216
+for b64_data in b64_cache:
+    svg_content += f'    <image x="{x_offset}" y="16" width="48" height="48" href="data:image/svg+xml;base64,{b64_data}" />\n'
+    x_offset += 64
+
+svg_content += """  </g>
+</svg>"""
 
 with open("assets/tech-carousel.svg", "w", encoding="utf-8") as f:
     f.write(svg_content)
 
-print("Successfully generated tech-carousel.svg!")
+print("Successfully generated tech-carousel.svg with Base64 images!")
